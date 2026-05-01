90bec715bbf926b3231efe17a2e1ab110d06cdad
dayanand77 <dayugm6262@gmail.com>
2026-05-01 23:42:51 +0530

    bug fixed again

--- backend/list_users.py
+++
@@ -1,9 +0,0 @@
-from app.core.db import get_db
-from app import create_app
-
-app = create_app()
-with app.app_context():
-    db = get_db()
-    users = list(db.users.find({}))
-    for u in users:
-        print(f"UID: {u.get('uid')}, Email: {u.get('email')}, Role: {u.get('role')}")
